92d0813afbe1223a0aeaab752815fba7a109c58b
Christophe Roblin <1260186+ChristopheRoblin@users.noreply.github.com>
2023-11-19 22:05:46 +0100

    Update entrypoint.sh

--- steamcmd/entrypoint.sh
+++ steamcmd/entrypoint.sh
@@ -65,7 +65,7 @@ fi
 # Replace Startup Variables
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
-
+pwd
 # Run the Server
 ${MODIFIED_STARTUP}
 
